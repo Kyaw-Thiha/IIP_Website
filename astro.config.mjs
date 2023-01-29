bdd7fc4334d0fbc28b658cf411c2a816aca5f157
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-01-29 23:43:30 +0630

    Configured i18next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroI18next()],
 });
