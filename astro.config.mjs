6f6af0e7cc25f42ca7a002d4c5a328a1fa3b9175
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-02-02 02:46:16 +0630

    Added in astro-imagetools plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()],
+  integrations: [tailwind(), astroI18next(), astroImageTools()],
 });
