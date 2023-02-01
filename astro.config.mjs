b0eef9e6979ed8f341443fcdd084cd7182b56921
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-02-02 02:51:27 +0630

    Added sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import astroI18next from "astro-i18next";
 import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools()],
+  integrations: [tailwind(), astroI18next(), astroImageTools(), sitemap()]
 });
