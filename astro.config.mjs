d0e13220557d255e020b5b1de3c585b9c4fd2455
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-02-15 01:07:59 +0630

    Removed react and added vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import { astroImageTools } from "astro-imagetools";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
 
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools, sitemap(), react()],
-  site: import.meta.env.DEV ? "http://localhost:3000" : "https://www.my-site.dev"
+  integrations: [tailwind(), astroI18next(), astroImageTools, sitemap(), vue()],
+  site: import.meta.env.DEV
+    ? "http://localhost:3000"
+    : "https://www.my-site.dev",
 });
