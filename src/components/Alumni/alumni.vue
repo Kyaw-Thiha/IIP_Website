88a28651d62675e449207ea84c529d0ea97730bc
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-03-14 23:52:24 +0630

    Created Alumni page

---
+++ src/components/Alumni/alumni.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, onMounted } from "vue";
+import { majors } from "./subjectsData";
+
+export default defineComponent({
+  name: "Alumni",
+  components: {},
+  setup() {
+    const variable = ref("");
+    const data = reactive([
+      {
+        name: "",
+      },
+    ]);
+
+    interface Alumni {
+      name: string;
+      year: number;
+      major: string;
+      subjects: string;
+    }
+
+    const method = () => {};
+
+    return { variable, method };
+  },
+});
+</script>
