7a452cae51671f4ba6ae44f3a9769c3aa8748e48
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-02 20:09:12 +0630

    Updated the @astrojs/image package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools, sitemap(), vue()],
-  site: import.meta.env.DEV
-    ? "http://localhost:3000"
-    : "https://www.my-site.dev",
+  integrations: [tailwind(), astroI18next(), astroImageTools, sitemap(), vue(), image()],
+  site: import.meta.env.DEV ? "http://localhost:3000" : "https://www.my-site.dev"
 });
