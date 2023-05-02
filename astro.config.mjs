d985a999ac65d1d48bd619ab7478843fdedd933b
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-03 00:24:32 +0630

    Configured for experimental astro images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
-import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), astroImageTools, sitemap(), vue(), image()],
-  site: import.meta.env.DEV ? "http://localhost:3000" : "https://www.my-site.dev"
+  integrations: [tailwind(), astroI18next(), sitemap(), vue()],
+  site: import.meta.env.DEV
+    ? "http://localhost:3000"
+    : "https://www.my-site.dev",
+  experimental: {
+    assets: true,
+  },
 });
