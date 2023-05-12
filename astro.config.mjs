6a476b15b760b076179aec44c3246cc6ef26c5f7
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-12 10:46:53 +0630

    Added react support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import { sharpImageService } from "astro/assets";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next(), sitemap(), vue()],
-  site: import.meta.env.DEV
-    ? "http://localhost:3000"
-    : "https://www.my-site.dev",
+  integrations: [tailwind(), astroI18next(), sitemap(), vue(), react()],
+  site: import.meta.env.DEV ? "http://localhost:3000" : "https://www.my-site.dev",
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: sharpImageService(),
-  },
+    service: sharpImageService()
+  }
 });
---
+++ src/components/Alumni/AlumniList.jsx
@@ -0,0 +1,5 @@
+function AlumniList() {
+  return <div>HI HI HI</div>;
+}
+
+export default AlumniList;
