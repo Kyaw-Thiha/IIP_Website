79b8c5f27ba7781178d3f5c52f36b49337997d5b
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-19 23:12:55 +0630

    Edited

--- src/components/Alumni/AlumniList.jsx
+++ src/components/Alumni/AlumniList.jsx
@@ -1,7 +1,23 @@
+import { Button } from "@components/ui/button";
+import { Input } from "@components/ui/input";
+
+import { SearchIcon } from "lucide-react";
+import "./Alumni.css";
+
 function AlumniList() {
   return (
-    <div className="m">
-      <input type="text" className="w-50%" />
+    <div className="flex w-full space-x-2 p-10 ">
+      <div className="flex w-6/12 flex-row">
+        <Input
+          className="height: rounded-none border-2 border-r-0 border-black focus-visible:ring-0"
+          type="email"
+          placeholder="Search"
+        />
+
+        <Button className="rounded-none border-2 border-l-0 border-black bg-white hover:bg-white">
+          <SearchIcon className="mx-0 transition-all hover:scale-x-125 hover:scale-y-125 " />
+        </Button>
+      </div>
     </div>
   );
 }
