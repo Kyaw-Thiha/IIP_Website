0cc603cf59e090bdff60d6e6290408adf57b5b02
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-19 10:54:38 +0630

    Updated sharp image service import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
-import { sharpImageService } from "astro/assets";
 
 import react from "@astrojs/react";
 
